f72a4ab01ce6d63376c671992bc3c399754d0095
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-09-18 02:04:11 +0000

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { Button, Loading, Table, Text } from '@nextui-org/react'
+import { Button, Loading, Table, Text, useAsyncList } from '@nextui-org/react'
 import { NextPage } from 'next'
 import Link from 'next/link'
-import useSWR from "swr"
+import useSWR from 'swr'
 import Router from 'next/router'
 
 type music = {
@@ -16,18 +16,27 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 const Home: NextPage = () => {
   const { data } = useSWR('/api/list', fetcher)
-  const musicList:Array<music> = data
+  const musicList: Array<music> = data
+
+  async function load() {
+    const res = await fetch('/api/list')
+    const json = (await res.json()) as Array<music>
+    return {
+      items: json,
+    }
+  }
+  const list = useAsyncList({ load })
   return (
     <>
       <Button css={{ margin: 10 }} onClick={() => Router.push('/create')}>
         追加
       </Button>
       {!data && <Loading />}
-      {musicList && (<Text css={{margin:10}}>現在{musicList.length}曲あります</Text>)}
+      {musicList && <Text css={{ margin: 10 }}>現在{musicList.length}曲あります</Text>}
       <Table
         aria-label='曲リスト'
         css={{
-          height: 'auto',
+          height: 'calc($space$14 * 10)',
           minWidth: '100%',
           margin: 20,
         }}
@@ -37,15 +46,17 @@ const Home: NextPage = () => {
           <Table.Column>作曲者</Table.Column>
           <Table.Column>ジャンル</Table.Column>
         </Table.Header>
-        <Table.Body>
-          {musicList &&
-            musicList.map((music, index) => (
-              <Table.Row key={index}>
-                <Table.Cell><a href={music.url}>{music.url}</a></Table.Cell>
-                <Table.Cell>{music.composer}</Table.Cell>
-                <Table.Cell>{music.genre}</Table.Cell>
-              </Table.Row>
-            ))}
+        <Table.Body items={list.items} loadingState={list.loadingState} onLoadMore={list.loadMore}>
+          {(item) => (
+            <Table.Row key={item.id}>
+                  <Table.Cell>
+                    <a href={item.url}>{item.url}</a>
+                  </Table.Cell>
+                  <Table.Cell>{item.composer}</Table.Cell>
+                  <Table.Cell>{item.genre}</Table.Cell>
+
+            </Table.Row>
+          )}
         </Table.Body>
       </Table>
     </>
